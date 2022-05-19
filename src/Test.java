41d538d7cf107399331f32af87efca07a09fa1e8
Gennady KotsenyaÐ <genkoc21@gmail.com>
2022-05-19 13:53:00 +0300

    add new Tusk4 1.0

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,14 @@
-public class Test {
 
+
+public class Test {
+    public static void main(String[] args) {
+
+
+        int r = 3;
+
+        String e = String.valueOf(r);
+
+        System.out.println(e);
+
+    }
 }
