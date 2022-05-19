7a0be100dcae0a7ba92fe9424a27fa0cb41c04bb
Gennady KotsenyaÐ <genkoc21@gmail.com>
2022-05-19 14:11:59 +0300

    add new Tusk4 1.1

--- src/GameTicTacToe/Task4.java
+++ src/GameTicTacToe/Task4.java
@@ -13,7 +13,7 @@ public class Task4 {
 
 //        int[][] grid = {{ 0, 2, 1 },
 //                        { 0, 2, 0 },
-//                        { 1, 2, 1 }};
+//                        { 1, 2, 1 }}
 
 
 //        int[][] grid = {{ 2, 2, 2 },
@@ -40,171 +40,79 @@ public class Task4 {
 //                        { 1, 2, 0 }};
 
 
-//        int[][] grid = {{1, 2, 2},
-//                        {2, 1, 1},
-//                        {0, 1, 2}};
+        int[][] grid = {{1, 1, 2},
+                        {2, 0, 1},
+                        {2, 0, 1}};
 
 //        int[][] grid = {{1, 1, 1, 1},
 //                        {2, 2, 2, 0},
 //                        {0, 0, 0, 0},
 //                        {0, 0, 0, 0}}  ;
 
-        int[][] grid = { {1, 0, 1, 0, 2},
-                         {0, 2, 0, 1, 0},
-                         {1, 0, 0, 2, 0},
-                         {0, 1, 0, 2, 0},
-                         {2, 0, 0, 0, 1} }  ;
+//        int[][] grid = { {1, 0, 1, 0, 2},
+//                         {0, 2, 0, 1, 0},
+//                         {1, 0, 0, 2, 0},
+//                         {0, 1, 0, 2, 0},
+//                         {2, 0, 0, 0, 1} }  ;
 
         System.out.println(determineWinner(grid));
     }
 
     public static int determineWinner(int[][] grid) {
 
-
-        int sumDiagonalToRightX = 0;
-        int sumDiagonalToRightO = 0;
-        int sumDiagonalToLeftX = 0;
-        int sumDiagonalToLeftO = 0;
-        int sumOfColumnX = 0;
-        int sumOfColumnO = 0;
-        int sumOfLineX = 0;
-        int sumOfLineO = 0;
-        int resultX = grid.length;
-        int resultO = 2 * grid.length;
-        boolean less = false;
+        int counterRepeatSymbol = 1;
+        int counterX = 0;
+        int counterO = 0;
+        int countDrawGameLine  = 0;
 
         int x = 1;
         int o = 2;
         int draw = -1;
 
-        int forDrawGame = x + o;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid.length; j++){
 
-        for (int i = 0; i < grid.length; i++) {
-            System.out.println("i = " + i);
-            System.out.println("sumOfColumnX, sumOfColumnO, forDrawGame = "+sumOfColumnX+ " , "+ sumOfColumnO +" , "+forDrawGame);
-            if(i != 0 && (sumOfColumnX >= x && sumOfColumnO >= o) && (sumOfColumnX != resultX && sumOfColumnO != resultO)){
-                less = true;
+                if(grid[i][j] == x){
+                    counterX++;
+                }
+                if(grid[i][j] == o){
+                    counterO++;
+                }
+
+                if(j != 0 && grid[i][j] == grid[i][j - 1]){
+                    counterRepeatSymbol++;
+                }
+
+
+                if(j == grid.length - 1) {
+                    if (counterRepeatSymbol == grid.length) {
+                        if (grid[i][j] == x) {
+                            return x;
+                        } else if (grid[i][j] == o) {
+                            return o;
+                        }
+
+
+                    } else if (counterX > 0 && counterO > 0) {
+                        countDrawGameLine++;
+                    }
+
+                    if( i == grid.length - 1 && countDrawGameLine == grid.length){
+
+                        return draw;
+                    }
+                    if(i == grid.length - 1){
+                        countDrawGameLine = 0;
+                    }
+                }
+
             }
-            sumOfColumnX = 0;
-            sumOfColumnO = 0;
-
-            for (int j = 0; j < grid.length; j++) {
-                System.out.println("j = " + j);
-
-                if (grid[j][i] == x) {
-                    System.out.println("sumOfColumnX += " + grid[j][i]);
-                    sumOfColumnX += grid[j][i];
-                    System.out.println("sumOfColumnX = " + sumOfColumnX);
-                } else if (grid[j][i] == o) {
-
-                    System.out.println("sumOfColumnO += " + grid[j][i]);
-                    sumOfColumnO += grid[j][i];
-                    System.out.println("sumOfColumnO = " + sumOfColumnO);
-
-                }
-
-                if (j == grid.length - 1) {
-                    System.out.println("return0");
-                    if (sumOfColumnX == resultX)
-                        return x;
-                    else if (sumOfColumnO == resultO)
-                        return o;
-                }
-            }
-        }
-
-        for (int i = 0; i < grid.length; i++) {
-            System.out.println("i = " + i);
-            sumOfLineX = 0;
-            sumOfLineO = 0;
-
-            for (int j = 0; j < grid[i].length; j++) {
-                System.out.println("j = " + j);
-
-                if (grid[i][j] == x) {
-                    System.out.println("if(grid[" + i + "][" + j + "] == 1) ");
-
-                    if (i == j) {
-                        System.out.println("sumDiagonalToRightX += " + grid[i][j]);
-                        sumDiagonalToRightX += grid[i][j];
-                        System.out.println("sumDiagonalToRightX = " + sumDiagonalToRightX);
-
-                    }
-
-                    if ((i + j) == grid.length - 1) {
-                        System.out.println("sumDiagonalToLeftX += " + grid[i][j]);
-                        sumDiagonalToLeftX += grid[i][j];
-                        System.out.println("sumDiagonalToLeftX = " + sumDiagonalToLeftX);
-
-                    }
-
-                    System.out.println("sumOfLineX += " + grid[i][j]);
-                    sumOfLineX += grid[i][j];
-                    System.out.println("sumOfLineX = " + sumOfLineX);
-
-                } else if (grid[i][j] == o) {
-                    System.out.println("grid[i][j]  == 2");
-
-
-                    if (i == j) {
-                        System.out.println("sumDiagonalToRightO += " + grid[i][j]);
-                        sumDiagonalToRightO += grid[i][j];
-                        System.out.println("sumDiagonalToRightO = " + sumDiagonalToRightO);
-
-                    }
-
-                    if ((i + j) == grid.length - 1) {
-                        System.out.println("sumDiagonalToLeftO += " + grid[i][j]);
-                        sumDiagonalToLeftO += grid[i][j];
-                        System.out.println("sumDiagonalToLeftO = " + sumDiagonalToLeftO);
-
-                    }
-                    System.out.println("sumOfLineO += " + grid[i][j]);
-                    sumOfLineO += grid[i][j];
-                    System.out.println("sumOfLineO = " + sumOfLineO);
-
-                }
-
-                if (i == grid.length - 1 && j == 0) {
-                    System.out.println("return1");
-
-                    if (sumDiagonalToLeftX == resultX)
-                        return x;
-                    else if (sumDiagonalToLeftO == resultO)
-                        return o;
-
-                }
-                if (j == grid.length - 1) {
-                    System.out.println("return2");
-                    if (sumOfLineX == resultX)
-                        return x;
-                    else if (sumOfLineO == resultO)
-                        return o;
-                }
-                if (i == grid.length - 1 && j == grid.length - 1) {
-                    System.out.println("return3");
-                    if (sumDiagonalToRightX == resultX)
-                        return x;
-                    else if (sumDiagonalToRightO == resultO)
-                        return o;
-                }
-
-                System.out.println("------cycle--------");
-            }
-
-
+            counterRepeatSymbol = 1;
+            counterX = 0;
+            counterO = 0;
 
         }
-
-        if (!less && (sumDiagonalToLeftO > forDrawGame || sumDiagonalToLeftX > forDrawGame) &&
-                (sumDiagonalToRightO > forDrawGame || sumDiagonalToRightX > forDrawGame) &&
-                (sumOfLineO > forDrawGame || sumOfLineX > forDrawGame) &&
-                (sumOfColumnO > forDrawGame || sumOfColumnX > forDrawGame)) {
-            return draw;
-
-        }
-
-
+        System.out.println("---------------");
         return 0;
     }
 }
