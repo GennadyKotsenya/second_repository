085e1edbda3576adc539e0cd31f1e882537b8e3c
Gennady KotsenyaÐ <genkoc21@gmail.com>
2022-05-22 14:55:54 +0300

    add Tusk4, final version

--- src/GameTicTacToe/Task4.java
+++ src/GameTicTacToe/Task4.java
@@ -2,58 +2,17 @@ package GameTicTacToe;
 
 public class Task4 {
     public static void main(String[] args) {
-//        int[][] grid = {{ 2, 0, 1 },
-//                        { 0, 2, 1 },
-//                        { 1, 2, 1 }};
+
+        int[][] grid = {{2, 1, 2},
+                        {2, 1, 1},
+                        {1, 2, 0}};
 
 
-//        int[][] grid = {{ 2, 0, 1 },
-//                        { 0, 2, 0 },
-//                        { 1, 2, 1 }};
-
-//        int[][] grid = {{ 0, 2, 1 },
-//                        { 0, 2, 0 },
-//                        { 1, 2, 1 }}
-
-
-//        int[][] grid = {{ 2, 2, 2 },
-//                        { 0, 1, 0 },
-//                        { 1, 0, 1 }};
-
-
-//        int[][] grid = {{ 2, 1, 2 },
-//                        { 0, 2, 1 },
-//                        { 1, 0, 2 }};
-
-
-//        int[][] grid = {{ 1, 0, 2 },
-//                        { 2, 1, 0 },
-//                        { 1, 2, 1 }};
-
-//        int[][] grid = {{ 1, 0, 2 },
-//                        { 1, 1, 0 },
-//                        { 1, 2, 2 }};
-
-
-//        int[][] grid = {{ 2, 0, 1 },
-//                        { 2, 1, 0 },
-//                        { 1, 2, 0 }};
-
-
-//        int[][] grid = {{2, 0, 1},
-//                        {0, 0, 1},
-//                        {2, 0, 1}};
-
-//        int[][] grid = {{1, 1, 1, 1},
-//                        {2, 2, 2, 0},
-//                        {0, 0, 0, 0},
-//                        {0, 0, 0, 0}}  ;
-
-        int[][] grid = { {1, 2, 0, 1, 2},
-                         {1, 0, 2, 0, 2},
-                         {2, 0, 0, 2, 0},
-                         {1, 0, 1, 0, 1},
-                         {1, 2, 0, 1, 0} }  ;
+//        int[][] grid = { {0, 2, 2, 1, 0},
+//                         {1, 0, 1, 0, 2},
+//                         {2, 1, 1, 2, 0},
+//                         {1, 2, 0, 2, 1},
+//                         {0, 2, 0, 1, 0} }  ;
 
         System.out.println(determineWinner(grid));
     }
@@ -65,107 +24,102 @@ public class Task4 {
         int x = 1;
         int o = 2;
         int draw = -1;
-
-//        for (int i = 0; i < grid.length; i++) {
-//            for (int j = 0; j < grid.length; j++) {
-//
-//                if (j != 0 && grid[i][j] == grid[i][j - 1]) {
-//                    counterRepeatSymbol++;
-//                }
-//
-//                if (j == grid.length - 1 && counterRepeatSymbol == grid.length) {
-//                    if (grid[i][j] == x) {
-//                        return x;
-//                    } else if (grid[i][j] == o) {
-//                        return o;
-//                    }
-//                }
-//            }
-//            counterRepeatSymbol = 1;
-//        }
-//
-//        for (int i = 0; i < grid.length; i++) {
-//            for (int j = 0; j < grid.length; j++) {
-//
-//                if (j != 0 && grid[j][i] == grid[j - 1][i]) {
-//                    counterRepeatSymbol++;
-//                }
-//
-//                if (j == grid.length - 1 && counterRepeatSymbol == grid.length) {
-//                    if (grid[j][i] == x) {
-//                        return x;
-//                    } else if (grid[j][i] == o) {
-//                        return o;
-//                    }
-//                }
-//
-//            }
-//
-//            counterRepeatSymbol = 1;
-//        }
-//
-//        for (int i = 0; i < grid.length; i++) {
-//            for (int j = 0; j < grid.length; j++) {
-//
-//                if(i == j) {
-//                    if (i != 0 && grid[i][j] == grid[i - 1][j - 1]) {
-//                        counterRepeatSymbol++;
-//                    }
-//
-//                    if (j == grid.length - 1 && counterRepeatSymbol == grid.length) {
-//                        if (grid[i][j] == x) {
-//                            return x;
-//                        } else if (grid[i][j] == o) {
-//                            return o;
-//                        }
-//                    }
-//                }
-//            }
-//
-//        }
-//        counterRepeatSymbol = 1;
-//
-//        for (int i = 0; i < grid.length; i++) {
-//            for (int j = 0; j < grid.length; j++) {
-//
-//                if(i == (grid.length - 1 - j)) {
-//                    if (i != 0 && grid[i][j] == grid[i - 1][j + 1]) {
-//                        counterRepeatSymbol++;
-//                    }
-//
-//                    if (i == grid.length - 1 && counterRepeatSymbol == grid.length) {
-//                        if (grid[i][j] == x) {
-//                            return x;
-//                        } else if (grid[i][j] == o) {
-//                            return o;
-//                        }
-//                    }
-//                }
-//            }
-//
-//        }
-//        counterRepeatSymbol = 1;
-
-        int counterX = 0;
-        int counterO = 0;
-        int counter = 0;
-
+        int uncertainty = 0;
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid.length; j++) {
 
-                if(grid[i][j] == x) {
-                    counterX++;
-                } else if(grid[i][j] == o) {
-                    counterO++;
-                }
-                if(j == grid.length - 1 && counterX > 0 && counterO > 0){
-                    counter++;
+                if (j != 0 && grid[i][j] == grid[i][j - 1]) {
+                    counterRepeatSymbol++;
                 }
 
-                if(counter == grid.length - 1){
-                    return draw;
+                if (j == grid.length - 1 && counterRepeatSymbol == grid.length) {
+                    if (grid[i][j] == x) {
+                        return x;
+                    } else if (grid[i][j] == o) {
+                        return o;
+                    }
                 }
+            }
+            counterRepeatSymbol = 1;
+        }
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+
+                if (j != 0 && grid[j][i] == grid[j - 1][i]) {
+                    counterRepeatSymbol++;
+                }
+
+                if (j == grid.length - 1 && counterRepeatSymbol == grid.length) {
+                    if (grid[j][i] == x) {
+                        return x;
+                    } else if (grid[j][i] == o) {
+                        return o;
+                    }
+                }
+
+            }
+
+            counterRepeatSymbol = 1;
+        }
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+
+                if (i == j) {
+                    if (i != 0 && grid[i][j] == grid[i - 1][j - 1]) {
+                        counterRepeatSymbol++;
+                    }
+
+                    if (j == grid.length - 1 && counterRepeatSymbol == grid.length) {
+                        if (grid[i][j] == x) {
+                            return x;
+                        } else if (grid[i][j] == o) {
+                            return o;
+                        }
+                    }
+                }
+            }
+
+        }
+        counterRepeatSymbol = 1;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+
+                if (i == (grid.length - 1 - j)) {
+                    if (i != 0 && grid[i][j] == grid[i - 1][j + 1]) {
+                        counterRepeatSymbol++;
+                    }
+
+                    if (i == grid.length - 1 && counterRepeatSymbol == grid.length) {
+                        if (grid[i][j] == x) {
+                            return x;
+                        } else if (grid[i][j] == o) {
+                            return o;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        int counterX = 0;
+        int counterO = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+
+                if (grid[i][j] == x) {
+                    counterX++;
+                } else if (grid[i][j] == o) {
+                    counterO++;
+                }
+                if (j == grid.length - 1 && (counterX < 1 || counterO < 1)) {
+                    return uncertainty;
+                }
+
             }
             counterX = 0;
             counterO = 0;
@@ -173,34 +127,64 @@ public class Task4 {
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid.length; j++) {
-                System.out.println("j,i = " + j +","+i);
 
-                if(grid[j][i] == x) {
+                if (grid[j][i] == x) {
                     counterX++;
                 }
-                if(grid[j][i] == o) {
+                if (grid[j][i] == o) {
                     counterO++;
                 }
-                if(j == grid.length - 1 && counterX > 0 && counterO > 0){
-                    counter++;
-                    System.out.println("counter = " + counter);
+                if (j == grid.length - 1 && (counterX < 1 || counterO < 1)) {
+                    return uncertainty;
                 }
 
-                if(counter == grid.length){
-                    return draw;
-                }
-                if(j == grid.length - 1){
+                if (j == grid.length - 1) {
                     counterX = 0;
                     counterO = 0;
                 }
             }
+        }
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+
+                if (i == j) {
+                    if (grid[i][j] == x) {
+                        counterX++;
+                    } else if (grid[i][j] == o) {
+                        counterO++;
+                    }
+
+                    if (j == grid.length - 1 && (counterX < 1 || counterO < 1)) {
+                        return uncertainty;
+                    }
+                }
+
+            }
+
+        }
+        counterX = 0;
+        counterO = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+
+                if (i == (grid.length - 1 - j)) {
+                    if (grid[i][j] == x) {
+                        counterX++;
+                    } else if (grid[i][j] == o) {
+                        counterO++;
+                    }
+
+                    if (i == grid.length - 1 && (counterX < 1 || counterO < 1)) {
+                        return uncertainty;
+                    }
+                }
+
+            }
 
         }
 
-
-
-
-        System.out.println("---------------");
-        return 0;
+        return draw;
     }
 }
